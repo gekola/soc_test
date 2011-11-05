553467541149c41d6bd16b9edb499ab2b36fa8e9
Egor Lynko <flexoid@gmail.com>
2011-11-05 14:15:22 +0300

    Added 'form' action to PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
-  
+
   def home
+  end
+
+  def form
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SocTest::Application.routes.draw do
 
+  get "pages/form"
+
   resources :questionaries
   resources :questions
   resources :answers, :only => [:new, :create, :destroy, :edit, :update]
   resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => 'pages#home'
+  match '/form', :to => 'pages#form'
 
   match '/signin',  :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
