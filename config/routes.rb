779ffff13df37a1e0af32af670e9fb8a6d0f1524
Egor Lynko <flexoid@gmail.com>
2011-11-15 18:38:01 +0300

    Same path for the form at first time and with errors

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ SocTest::Application.routes.draw do
   resources :questionaries
   resources :questions
   resources :answers, :only => [:new, :create, :destroy, :edit, :update]
-  resources :results, :only => [:new, :create, :index]
+  resources :results, :only => [:index]
   resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => 'pages#home'
 
-  match '/form', :to => 'results#new'
+  match '/form', :to => 'results#new', :via => :get
+  match '/form', :to => 'results#create', :via => :post
   match '/thanks', :to => 'pages#thanks'
 
   match '/signin',  :to => 'sessions#new'
