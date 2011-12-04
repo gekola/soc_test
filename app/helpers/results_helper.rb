887b84310410075ed7bfaf91d6100f62949e6330
Nick Herman <gerkola@gmail.com>
2011-12-04 03:43:26 +0300

    Legend added

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,21 +1,21 @@
 module ResultsHelper
 
   def getstatistics(par)
-#(questionary, a_id=0, filter={})
+#(questionary, a_id=0)
 
     questionary = par[:q]
-    a_id = par[:a]
-    filter = par[:f]
-    if filter.nil? || filter.length == 0
-      filtered = questionary.results.map { |res| res.information }
-    else
-      filtered = questionary.results.map do |res|
-        res.information if filter.inject(false) do |r,i|
-          r || (res.information.member? i)
-        end
-      end.compact
+    @a_id = par[:a]
+    #filter = par[:f]
+    filtered = questionary.results.map { |res| res.information }
+    filtered.map! do |inf|
+      if filter.inject(false) { |r,i| (r || (inf.member? i)) } || filter == []
+        inf
+      else
+        nil
+      end
     end
-    return filtered.inject(0) { |count,inf| (inf.member? a_id) || (a_id == 0) ? count+1: count }
+    filtered.compact!
+    return (filtered.inject(0) { |count,inf| ((inf.member? @a_id) || (@a_id == 0)) ? count+1 : count })
   end
 
   def numOfColumns(questionary)
@@ -26,13 +26,33 @@ module ResultsHelper
     end
   end
 
+  def chparams(ans)
+
+    param = params.clone
+    if filter.member? ans
+      param[:count] = filter.length - 1
+      filter.inject(0) do |i,ans_id|
+        if ans_id != ans
+          param["a_#{i}".to_sym] = ans_id
+          i+1
+        else
+          i
+        end
+        param["a_#{filter.length-1}"] = nil
+      end
+    else
+      param = param.merge({ :count => filter.length+1, "a_#{filter.length}".to_sym => ans })
+    end
+    param
+  end
+
   def filter
 
     if params[:count].nil? || (params[:count] == 0)
-      nil
+      []
     else
       (0...params[:count].to_i).map do |i|
-        params["a_#{i}".to_sym]
+        params["a_#{i}".to_sym].to_i
       end
     end
   end
